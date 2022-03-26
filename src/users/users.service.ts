a9f8db83d49ab3a2aa60f103912ba6363978ac2a
DENIS LUKIN <nash1q@yandex.ru>
2022-03-26 12:09:02 +0300

    Added User Model

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { User } from "./users.model";
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User) private userRepository: typeof User) {}
+
+  async createUser(dto: CreateUserDto) {
+    return this.userRepository.create(dto);
+  }
+
+  async getAllUsers() {
+    return this.userRepository.findAll()
+  }
+}
