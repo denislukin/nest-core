9f5b19a34c2ae959ac893599b0cf6a4e6b874910
DENIS LUKIN <nash1q@yandex.ru>
2022-10-03 16:33:40 +0300

    feature/update files

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,21 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { CreateUserDto } from "src/users/dto/create-user.dto";
-import { AuthService } from "./auth.service";
+import {Body, Controller, Post} from '@nestjs/common';
+import {ApiTags} from "@nestjs/swagger";
+import {CreateUserDto} from "../users/dto/create-user.dto";
+import {AuthService} from "./auth.service";
 
-@Controller("auth")
+@ApiTags('Авторизация')
+@Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
 
-  @Post("/login")
-  login(@Body() userDto: CreateUserDto) {
-    this.authService.login(userDto);
-  }
+    constructor(private authService: AuthService) {}
 
-  @Post("registration")
-  registration(@Body() userDto: CreateUserDto) {
-    this.authService.registration(userDto);
-  }
+    @Post('/login')
+    login(@Body() userDto: CreateUserDto) {
+        return this.authService.login(userDto)
+    }
+
+    @Post('/registration')
+    registration(@Body() userDto: CreateUserDto) {
+        return this.authService.registration(userDto)
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,44 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { CreateUserDto } from "src/users/dto/create-user.dto";
-import { UsersService } from "src/users/users.service";
+import {HttpException, HttpStatus, Injectable, UnauthorizedException} from '@nestjs/common';
+import {CreateUserDto} from "../users/dto/create-user.dto";
+import {UsersService} from "../users/users.service";
+import {JwtService} from "@nestjs/jwt";
+import * as bcrypt from 'bcrypt'
+import {User} from "../users/users.model";
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
-  async login(userDto: CreateUserDto) {}
-  async registration(userDto: CreateUserDto) {
-    try {
-      const condidate = await this.userService.getUserByEmail(userDto.email);
-      if (condidate) {
-        throw new HttpException(
-          "User with this email already existed",
-          HttpStatus.BAD_REQUEST
-        );
-      }
-    } catch (e) {}
-  }
+
+    constructor(private userService: UsersService,
+        private jwtService: JwtService) {}
+
+    async login(userDto: CreateUserDto) {
+        const user = await this.validateUser(userDto)
+        return this.generateToken(user)
+    }
+
+    async registration(userDto: CreateUserDto) {
+        const candidate = await this.userService.getUserByEmail(userDto.email);
+        if (candidate) {
+            throw new HttpException('Пользователь с таким email существует', HttpStatus.BAD_REQUEST);
+        }
+        const hashPassword = await bcrypt.hash(userDto.password, 5);
+        const user = await this.userService.createUser({...userDto, password: hashPassword})
+        return this.generateToken(user)
+    }
+
+    private async generateToken(user: User) {
+        const payload = {email: user.email, id: user.id, roles: user.roles}
+        return {
+            token: this.jwtService.sign(payload)
+        }
+    }
+
+    private async validateUser(userDto: CreateUserDto) {
+        const user = await this.userService.getUserByEmail(userDto.email);
+        const passwordEquals = await bcrypt.compare(userDto.password, user.password);
+        if (user && passwordEquals) {
+            return user;
+        }
+        throw new UnauthorizedException({message: 'Некорректный емайл или пароль'})
+    }
 }
